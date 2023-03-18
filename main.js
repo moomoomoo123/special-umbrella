70b75afc5705e6ddb0045225256f27f14b162eeb
moomoomoo123 <106563335+moomoomoo123@users.noreply.github.com>
2023-03-17 18:25:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+twice = "";
+bts = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload() {
+    song = loadSound("BTS.mp3");
+    song = loadSound("TWICE.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded() {
+    console.log("Posenet is Initialized!")
+}
+
+function gotPoses() {
+   if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("LeftWristX = " + leftWristX + "LeftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("RightWristX = " + rightWristX + "RightWristY = " + rightWristY);
+   }
+}
